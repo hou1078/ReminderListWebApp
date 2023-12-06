663f81b80c04a563ddb10ed1802670e1e481675c
Your Name <you@example.com>
2023-12-06 02:11:15 -0800

    Finish all functionalities of the web app

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,13 +3,26 @@ module.exports = {
       if (req.isAuthenticated()) {
         return next();
       }
-      res.redirect("/login");
+      res.redirect("/auth/login");
     },
     forwardAuthenticated: function (req, res, next) {
       if (!req.isAuthenticated()) {
         return next();
       }
-      res.redirect("/login");
+      res.redirect("/auth/login");
     },
   };
   
+
+
+//   const authMiddleware = require('./path-to-auth-middleware');
+
+// // 使用 ensureAuthenticated 中间件确保用户通过身份验证
+// app.get('/secure-page', authMiddleware.ensureAuthenticated, (req, res) => {
+//   // 处理已通过身份验证的用户
+// });
+
+// // 使用 forwardAuthenticated 中间件确保用户未通过身份验证
+// app.get('/login', authMiddleware.forwardAuthenticated, (req, res) => {
+//   // 处理未通过身份验证的用户
+// });
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
 
+//------Welcome Router---------///
 router.get("/", (req, res) => {
   res.send("welcome");
 });
 
-router.get("/reminder", ensureAuthenticated, (req, res) => {
-  res.render("reminder", {
+
+//-----------reminder dashboard------------//
+router.get("/reminders", ensureAuthenticated, (req, res) => {
+  res.render("reminders", {
     user: req.user,
   });
 });
